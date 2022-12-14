343fbbb3ebb21f9688902e2cfa24bfda71b31e76
Sushant <83469632+sushant91265@users.noreply.github.com>
2022-12-14 17:22:57 +0200

    add more unit tests

--- server/tests/app/jobs/test_worker.py
+++ server/tests/app/jobs/test_worker.py
@@ -1,5 +1,7 @@
 import sys
 import unittest
+from unittest.mock import patch
+from server.db.models import Task, Result
 sys.path.append('.')
 
 from app.jobs import worker
@@ -12,6 +14,98 @@ class TestWorker(unittest.TestCase):
     def test_extract_numbers_empty_case(self):
         result = worker._extract_numbers([])
         self.assertEqual(result, [])
+    
+    def test_save_processed_numbers_positive_case(self):
+        # mock db
+        with patch('app.jobs.worker.db') as mock_db:
+            # mock session
+            with patch('app.jobs.worker.db.session') as mock_session:
+                # mock task
+                with patch('app.jobs.worker.db.session.query') as mock_query:
+                    mock_query.return_value.filter.return_value.first.return_value = Task()
+                    # mock result
+                    with patch('app.jobs.worker.db.session.query') as mock_query:
+                        mock_query.return_value.filter.return_value.first.return_value = Result()
+                        worker._save_processed_numbers("123", ["+491234567890", "00491234567891"])
+                        mock_query.return_value.filter.return_value.first.return_value.save.assert_called_once()
+
+    def test_save_processed_numbers_empty_case(self):
+        # mock db
+        with patch('app.jobs.worker.db') as mock_db:
+            # mock session
+            with patch('app.jobs.worker.db.session') as mock_session:
+                # mock task
+                with patch('app.jobs.worker.db.session.query') as mock_query:
+                    mock_query.return_value.filter.return_value.first.return_value = Task()
+                    # mock result
+                    with patch('app.jobs.worker.db.session.query') as mock_query:
+                        mock_query.return_value.filter.return_value.first.return_value = None
+                        worker._save_processed_numbers("123", ["+491234567890", "00491234567891"])
+                        mock_query.return_value.filter.return_value.first.return_value.save.assert_not_called()
+
+    def test_tasks_to_process_positive_case(self):
+        # mock db
+        with patch('app.jobs.worker.db') as mock_db:
+            # mock session
+            with patch('app.jobs.worker.db.session') as mock_session:
+                # mock task
+                with patch('app.jobs.worker.db.session.query') as mock_query:
+                    mock_query.return_value.filter.return_value.limit.return_value = [Task()]
+                    # mock result
+                    with patch('app.jobs.worker.db.session.query') as mock_query:
+                        mock_query.return_value.filter.return_value.first.return_value = Result()
+                        result = worker._tasks_to_process()
+                        self.assertEqual(len(result), 1)
+
+    def test_tasks_to_process_empty_case(self):
+        # mock db
+        with patch('app.jobs.worker.db') as mock_db:
+            # mock session
+            with patch('app.jobs.worker.db.session') as mock_session:
+                # mock task
+                with patch('app.jobs.worker.db.session.query') as mock_query:
+                    mock_query.return_value.filter.return_value.limit.return_value = []
+                    # mock result
+                    with patch('app.jobs.worker.db.session.query') as mock_query:
+                        mock_query.return_value.filter.return_value.first.return_value = None
+                        result = worker._tasks_to_process()
+                        self.assertEqual(len(result), 0)
+
+    def test_process_task_positive_case(self):
+        # mock db
+        with patch('app.jobs.worker.db') as mock_db:
+            # mock session
+            with patch('app.jobs.worker.db.session') as mock_session:
+                # mock task
+                with patch('app.jobs.worker.db.session.query') as mock_query:
+                    mock_query.return_value.filter.return_value.first.return_value = Task()
+                    # mock result
+                    with patch('app.jobs.worker.db.session.query') as mock_query:
+                        mock_query.return_value.filter.return_value.first.return_value = Result()
+                        # mock worker
+                        with patch('app.jobs.worker._extract_numbers') as mock_extract_numbers:
+                            mock_extract_numbers.return_value = ["+491234567890", "00491234567891"]
+                            with patch('app.jobs.worker._save_processed_numbers') as mock_save_processed_numbers:
+                                worker._process_task(Task())
+                                mock_save_processed_numbers.assert_called_once()
+
+    def test_process_task_empty_case(self):
+        # mock db
+        with patch('app.jobs.worker.db') as mock_db:
+            # mock session
+            with patch('app.jobs.worker.db.session') as mock_session:
+                # mock task
+                with patch('app.jobs.worker.db.session.query') as mock_query:
+                    mock_query.return_value.filter.return_value.first.return_value = None
+                    # mock result
+                    with patch('app.jobs.worker.db.session.query') as mock_query:
+                        mock_query.return_value.filter.return_value.first.return_value = None
+                        # mock worker
+                        with patch('app.jobs.worker._extract_numbers') as mock_extract_numbers:
+                            mock_extract_numbers.return_value = ["+491234567890", "00491234567891"]
+                            with patch('app.jobs.worker._save_processed_numbers') as mock_save_processed_numbers:
+                                worker._process_task(Task())
+                                mock_save_processed_numbers.assert_not_called()
 
 if __name__ == '__main__':
     unittest.main()
--- server/tests/app/services/test_metadata_service.py
+++ server/tests/app/services/test_metadata_service.py
@@ -1,47 +1,31 @@
 import unittest
 from unittest.mock import patch
-from app.services.metadata_service import MetadataService
+from server.app.services.metadata_service import MetadataService
 
 class TestMetadataService(unittest.TestCase):
-    @patch('app.services.metadata_service.MetadataService.create')
-    def test_create(self, mock_create):
-        mock_create.return_value = {'metadata': '1234'}
-        metadata_service = MetadataService(None, None)
-        metadata = metadata_service.create(None, None)
-        self.assertEqual(metadata, {'metadata': '1234'})
-        mock_create.assert_called_once()
-
-    @patch('app.services.metadata_service.MetadataService.list')
-    def test_list(self, mock_list):
-        mock_list.return_value = ['1234']
-        metadata_service = MetadataService(None, None)
-        metadatas = metadata_service.list()
-        self.assertEqual(metadatas, ['1234'])
-        mock_list.assert_called_once()
-
-    @patch('app.services.metadata_service.MetadataService.get')
-    def test_get(self, mock_get):
-        mock_get.return_value = ['1234']
-        metadata_service = MetadataService(None, None)
-        metadata = metadata_service.get(None)
-        self.assertEqual(metadata, ['1234'])
-        mock_get.assert_called_once()
-
-    @patch('app.services.metadata_service.MetadataService.update')
-    def test_update(self, mock_update):
-        mock_update.return_value = {'status': 'success'}
-        metadata_service = MetadataService(None, None)
-        response = metadata_service.update(None, None)
-        self.assertEqual(response, {'status': 'success'})
-        mock_update.assert_called_once()
-
-    @patch('app.services.metadata_service.MetadataService.delete')
+    def setUp(self):
+        self.client = MetadataService(None)
+        self.key = "test"
+        
+    @patch('server.app.services.metadata_service.MetadataService.save_task')
+    def test_save_task(self, mock_save_task):
+        self.client.save_task(self.key)
+        mock_save_task.assert_called_once_with(self.key)
+    
+    @patch('server.app.services.metadata_service.MetadataService.get_tasks')
+    def test_get_tasks(self, mock_get_tasks):
+        self.client.get_tasks()
+        mock_get_tasks.assert_called_once_with()
+    
+    @patch('server.app.services.metadata_service.MetadataService.get_results')
+    def test_get_results(self, mock_get_results):
+        self.client.get_results(self.key)
+        mock_get_results.assert_called_once_with(self.key)
+    
+    @patch('server.app.services.metadata_service.MetadataService.delete')
     def test_delete(self, mock_delete):
-        mock_delete.return_value = {'status': 'success'}
-        metadata_service = MetadataService(None, None)
-        response = metadata_service.delete(None)
-        self.assertEqual(response, {'status': 'success'})
-        mock_delete.assert_called_once()
+        self.client.delete(self.key)
+        mock_delete.assert_called_once_with(self.key)
 
 if __name__ == '__main__':
     unittest.main()
